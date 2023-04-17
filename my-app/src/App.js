ae6935d3dbf36f177e174247ba961c4ee14784de
PeetuVainio <peetu.vainio@gmail.com>
2023-04-17 12:48:36 +0300

    muokkaus

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,43 +1,105 @@
-import './App.css';
-import React, { useState } from 'react';
-import data from "./data.json";
-import Header from "./Header";
-import ToDoList from "./ToDoList";
-import ToDoForm from './ToDoForm';
+import React from "react";
+import "./App.css";
 
-function App() {
-  
-  const [ toDoList, setToDoList ] = useState(data);
+const App = () => {
+  const [todos, setTodos] = React.useState([]);
+  const [todo, setTodo] = React.useState("");
+  const [todoEditing, setTodoEditing] = React.useState(null);
+  const [editingText, setEditingText] = React.useState("");
 
-  const handleToggle = (id) => {
-    let mapped = toDoList.map(task => {
-      return task.id === Number(id) ? { ...task, complete: !task.complete } : { ...task};
+  React.useEffect(() => {
+    const json = localStorage.getItem("todos");
+    const loadedTodos = JSON.parse(json);
+    if (loadedTodos) {
+      setTodos(loadedTodos);
+    }
+  }, []);
+
+  React.useEffect(() => {
+    const json = JSON.stringify(todos);
+    localStorage.setItem("todos", json);
+  }, [todos]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newTodo = {
+      id: new Date().getTime(),
+      text: todo,
+      completed: false,
+    };
+    setTodos([...todos].concat(newTodo));
+    setTodo("");
+  }
+
+  function deleteTodo(id) {
+    let updatedTodos = [...todos].filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  }
+
+  function toggleComplete(id) {
+    let updatedTodos = [...todos].map((todo) => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
     });
-    setToDoList(mapped);
+    setTodos(updatedTodos);
   }
 
-  const handleFilter = () => {
-    let filtered = toDoList.filter(task => {
-      return !task.complete;
+  function submitEdits(id) {
+    const updatedTodos = [...todos].map((todo) => {
+      if (todo.id === id) {
+        todo.text = editingText;
+      }
+      return todo;
     });
-    setToDoList(filtered);
+    setTodos(updatedTodos);
+    setTodoEditing(null);
   }
 
-  const addTask = (userInput ) => {
-    let copy = [...toDoList];
-    copy = [...copy, { id: toDoList.length + 1, task: userInput, complete: false }];
-    setToDoList(copy);
-  }
+  return (
+    <div id="todo-list">
+      <h1>To Do Lista</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="teksti"
+          onChange={(e) => setTodo(e.target.value)}
+          value={todo}
+        />
+        <button type="submit">Lisää tehtävä</button>
+      </form>
+      {todos.map((todo) => (
+        <div key={todo.id} className="todo">
+          <div className="todoTeksti">
+            <input
+              type="checkbox"
+              id="completed"
+              checked={todo.completed}
+              onChange={() => toggleComplete(todo.id)}
+            />
+            {todo.id === todoEditing ? (
+              <input
+                type="teksti"
+                onChange={(e) => setEditingText(e.target.value)}
+              />
+            ) : (
+              <div>{todo.text}</div>
+            )}
+          </div>
+          <div className="todoActions">
+            {todo.id === todoEditing ? (
+              <button onClick={() => submitEdits(todo.id)}>Valmis</button>
+            ) : (
+              <button onClick={() => setTodoEditing(todo.id)}>Muokkaa</button>
+            )}
 
-  return (  
-    <div className="App">
-      <body>
-      <Header />
-      <ToDoForm addTask={addTask}/>
-      <ToDoList toDoList={toDoList} handleToggle={handleToggle} handleFilter={handleFilter}/>
-      </body>
+            <button onClick={() => deleteTodo(todo.id)}>Poista</button>
+          </div>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
