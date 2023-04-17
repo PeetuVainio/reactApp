16572c170ab4dc7f6347773a99e1111db9ebc981
PeetuVainio <peetu.vainio@edu.salpaus.fi>
2023-04-17 15:52:35 +0300

    background

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -98,6 +98,10 @@ const App = () => {
           </div>
         </div>
       ))}
+      <div class="d-flex flex-column justify-content-center w-100 h-100">
+      <div class="d-flex flex-column justify-content-center align-items-center">
+      </div>
+      </div>
     </div>
   );
 };
